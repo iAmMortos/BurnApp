06ee3de58159e1dfe5da9156ff45a98d12008690
Taylor Lopez <taylor.d.lopez@lmco.com>
2019-10-10 12:36:35 -0600

    Reorganizing resources to clean up

--- fire_view.py
+++ fire_view.py
@@ -8,10 +8,10 @@ class FireView (ui.View):
 	def did_load(self):
 		self.fire_wv = self['fire_wv']
 		self.touch_layer = self['touch_layer']
-		filename = os.path.abspath('fire.html')
+		filename = os.path.abspath('html/fire.html')
 		self.fire_wv.load_url(filename)
 		self.background_color = '#000'
-		self.airhorn_sound = sound.play_effect('airhorn.m4a')
+		self.airhorn_sound = sound.play_effect('rsc/airhorn.m4a')
 		self.touch_layer.down_action = self.handle_touch
 		
 	def will_close(self):
@@ -21,7 +21,7 @@ class FireView (ui.View):
 	def handle_touch(self, event):
 		if self.airhorn_sound:
 			self.airhorn_sound.stop()
-		self.airhorn_sound = sound.play_effect('airhorn_single.m4a')
+		self.airhorn_sound = sound.play_effect('rsc/airhorn_single.m4a')
 	
 	@staticmethod
 	def load_view():
